1d47c53816f0e6f95a4e23ddd63332ffa2e02e13
Venkata Duriseti <dur622@txml-2bqg8wp.ameritrade.com>
2017-10-14 18:21:26 -0500

    Adding files

---
+++ src/main/java/MathService.java
@@ -0,0 +1,10 @@
+public class MathService {
+
+    /*
+        Without using loops
+     */
+    public float power(int x, int n) {
+        //TODO
+        return 0.0f;
+    }
+}
---
+++ src/test/java/MathTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MathTest {
+    private MathService mathService;
+
+    @Before
+    public void setup() {
+        mathService = new MathService();
+    }
+
+    @Test
+    public void testPowerNormal(){
+        Assert.assertEquals(mathService.power(2,3), 8.0f, 0.001);
+    }
+
+    @Test
+    public void testPowerZero(){
+        Assert.assertEquals(mathService.power(2,0), 1.0f, 0.001);
+    }
+
+    @Test
+    public void testPowerOne(){
+        Assert.assertEquals(mathService.power(2,1), 2.0f, 0.001);
+    }
+
+    @Test
+    public void testPowerNegatives2(){
+        Assert.assertEquals(mathService.power(-2,3), -8.0f, 0.001);
+    }
+
+    @Test
+    public void testPowerNegatives(){
+        Assert.assertEquals(mathService.power(2,-3), 0.125, 0.001);
+    }
+
+}
