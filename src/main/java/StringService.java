cc8fd7f0288e4966c6459c9530c658e41b451fc4
Zippy <zippy@tos.com>
2017-10-17 22:36:28 -0500

    adding another problem

--- src/main/java/StringService.java
+++ src/main/java/StringService.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class StringService {
+
+    //Ignore symbols like . @ and spaces
+    public char findFirstNonRepeatingChar(String aString) {
+        throw new UnsupportedOperationException("Needs to be implemented");
+    }
+
 }
--- src/test/java/StringFirstNonRepeatingCharTest.java
+++ src/test/java/StringFirstNonRepeatingCharTest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class StringFirstNonRepeatingCharTest {
+
+    private StringService stringService;
+
+    @Before
+    public void setUp(){
+        this.stringService = new StringService();
+    }
+
+    @Test
+    public void testOne() {
+        Assert.assertEquals(stringService.findFirstNonRepeatingChar("twitter"), 'w');
+    }
+
+    @Test
+    public void testTwo() {
+        Assert.assertEquals(stringService.findFirstNonRepeatingChar("array"), 'y');
+    }
+
+    @Test
+    public void testThree() {
+        Assert.assertEquals(stringService.findFirstNonRepeatingChar("www is big!"), 's');
+    }
+
+    @Test
+    public void testFour() {
+        Assert.assertEquals(stringService.findFirstNonRepeatingChar("aaa@bbb.com"), 'c');
+    }
 }
